aea7b74cb55df747bdbc1c07d4aae1c5ded7c20e
Benjamin Kies <ben@joingrouper.com>
2016-09-01 18:58:32 -0400

    Add random and show endpoints

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -2,4 +2,13 @@ class ChallengesController < ApplicationController
   def index
     render json: Challenge.all
   end
+
+  def show
+    render json: Challenge.find(params[:id])
+  end
+
+  # get /api/challenges/random?level=
+  def random
+    render json: Challenge.where(level: params[:level]).all.sample
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   scope :api do
-    resources :challenges
+    resources :challenges do
+      collection do
+        get :random
+      end
+    end
     resources :responses
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
